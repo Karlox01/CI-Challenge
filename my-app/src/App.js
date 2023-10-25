bb8578accc23fd908e5d2dab8460d06ba85ee025
Karlox01 <orlandinikarlo@gmail.com>
2023-10-25 20:04:14 +0000

    done challenge 4

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,17 @@
 import css from './App.module.css';
 import SideBar from "./components/Sidebar";
 import NavBarSimple from './components/NavBarSimple';
+import NavBarForm from './components/NavBarForm';
 
 function App() {
   return (
     <div className={css.App}>
       <SideBar />
       <NavBarSimple />
+      <NavBarForm />
     </div>
   );
 }
 
 export default App;
+
